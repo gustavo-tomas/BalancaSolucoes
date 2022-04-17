3479955afc8028931c2b8f1e648689a3dace780d
Gustavo Tomás <guga.tomas10@gmail.com>
2022-04-17 10:59:57 -0300

    removed old files

--- index.js
+++
@@ -1,6 +0,0 @@
-// const test = document.querySelector('footer');
-
-// test.addEventListener('click', evt => {
-//   evt.preventDefault();
-//   test.style.setProperty('background-color', 'green');
-// });
