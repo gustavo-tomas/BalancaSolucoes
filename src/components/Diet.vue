498fd4b807f63a0371f5af67131f1416f2290e95
Gustavo Tomás <guga.tomas10@gmail.com>
2022-04-18 22:24:33 -0300

    split components in diets/exercise

---
+++ src/components/Diet.vue
@@ -0,0 +1,21 @@
+<template>
+-
+<!-- <table>
+  <thead :v-for="(value, key) in object"> 
+    <tr>{{ value }}</tr>
+    <tr>{{ key }}</tr>
+  </thead>
+</table> -->
+</template>
+
+<script>
+export default {
+  props: {
+    text: JSON,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Exercise.vue
@@ -0,0 +1,18 @@
+<template>
+  <table>
+    <thead>HEAD</thead>  
+  </table>
+</template>
+
+<script>
+
+export default {
+  props: {
+    text: String,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
