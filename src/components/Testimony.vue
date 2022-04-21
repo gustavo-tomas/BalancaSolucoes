a52777fc9029b24737b83b8a3cd68aa97d486c96
Gustavo Tomás <guga.tomas10@gmail.com>
2022-04-20 22:05:13 -0300

    started testimonies

---
+++ src/components/Testimony.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="testimony">
+    <p>{{ testName }}</p>
+    <img
+      :src="require(`../../public/assets/images/${testImg}`)"
+      class="testimony-image"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Testimony",
+  props: {
+    testName: String,
+    testImg: String,
+  },
+};
+</script>
+
+<style>
+.testimony {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  margin: auto;
+  width: 300px;
+  height: 150px;
+  background-color: red;
+}
+
+.testimony-image {
+  width: 100px;
+  height: 100px;
+  border-radius: 15px;
+}
+</style>
---
+++ src/views/Testimonies.vue
@@ -0,0 +1,22 @@
+<template>
+  <p class="title">O que dizem sobre nós</p>
+  <Testimony testName="Pelé" testImg="abacaxi.png" />
+</template>
+
+<script>
+import Testimony from "../components/Testimony.vue";
+
+export default {
+  name: "Testimonies",
+  components: {
+    Testimony,
+  },
+};
+</script>
+
+<style>
+.title {
+  font-size: 64px;
+  margin: 30px 0px;
+}
+</style>
